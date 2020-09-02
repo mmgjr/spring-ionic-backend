92429f064cf7bb2b359571ee1cee65a4d0d1f126
Mucio M G Junior <mlk_mucio@hotmail.com>
2020-09-02 16:08:58 -0300

    Get, Delete e Put do cliente

--- src/main/java/com/muciomgjr/cursomc/services/ClienteService.java
+++ src/main/java/com/muciomgjr/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.muciomgjr.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.muciomgjr.cursomc.domain.Cliente;
+import com.muciomgjr.cursomc.dto.ClienteDTO;
 import com.muciomgjr.cursomc.repositories.ClienteRepository;
+import com.muciomgjr.cursomc.services.exceptions.DataIntegrityException;
 import com.muciomgjr.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -22,5 +29,41 @@ public class ClienteService {
 				"Objeto não encontrado! Id:" + id + ", Tipo: "+Cliente.class.getName()
 				));
 
+	}
+	
+	
+	public Cliente update(Cliente obj) {
+		Cliente objDB = find(obj.getId());
+		updateData(objDB, obj);
+		return cliRepo.save(objDB);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			cliRepo.deleteById(id);			
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não foi possível excluir o cliente,pois ele possui pedidos.");
+		}
+	}
+	
+	public List<Cliente> findAll() {
+		return cliRepo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return cliRepo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+	
+	
+	
+	private void updateData(Cliente objDB,Cliente obj) {
+		objDB.setNome(obj.getNome());
+		objDB.setEmail(obj.getEmail());
 	}
 }
